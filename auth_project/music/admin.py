ba5ad5680ccaf93838c48c9f7b415560abbe0491
BatoolBtoush <batoolbtoush98@gmail.com>
2022-05-30 00:48:07 +0300

    jwt

--- auth_project/music/admin.py
+++ auth_project/music/admin.py
@@ -4,6 +4,33 @@ from .models import Music
 # Register your models here.
 @admin.register(Music)
 class MusicAdmin(admin.ModelAdmin):
-    list_display = ('title', 'artist', 'album', 'genre', 'year', 'timestamp', 'updated', 'user')
-    list_filter = ('title', 'artist', 'album', 'genre', 'year', 'timestamp', 'updated', 'user')
-    search_fields = ('title', 'artist', 'album', 'genre', 'year', 'timestamp', 'updated', 'user')
+    list_display = (
+        "title",
+        "artist",
+        "album",
+        "genre",
+        "year",
+        "timestamp",
+        "updated",
+        "user",
+    )
+    list_filter = (
+        "title",
+        "artist",
+        "album",
+        "genre",
+        "year",
+        "timestamp",
+        "updated",
+        "user",
+    )
+    search_fields = (
+        "title",
+        "artist",
+        "album",
+        "genre",
+        "year",
+        "timestamp",
+        "updated",
+        "user",
+    )
--- auth_project/music/models.py
+++ auth_project/music/models.py
@@ -14,5 +14,3 @@ class Music(models.Model):
 
     def __str__(self):
         return self.title
-
-        
